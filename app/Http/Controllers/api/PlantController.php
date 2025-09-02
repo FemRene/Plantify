c1bbe61a9f80f2d26ae422331e0041c542700f58
FemRene <you@example.com>
2025-09-02 07:08:31 +0200

    Initialize Laravel Livewire starter kit with default configuration and setup.

---
+++ app/Http/Controllers/api/PlantController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Plant;
+use Illuminate\Http\Request;
+
+class PlantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Plant::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Plant::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Plant::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return Plant::find($id)->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return Plant::find($id)->delete();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Http\Controllers\api\PlantController;
+
+Route::apiResource('plants', PlantController::class);
